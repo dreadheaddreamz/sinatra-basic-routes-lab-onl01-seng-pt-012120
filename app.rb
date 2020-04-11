1fa61697c1062c3aa822e20e8de491fb3b0859e5
James Batsell <jamesbatsell05@gmail.com>
2020-04-11 02:27:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
+  get '/name' do
+    "My name is James"
+  end
+  
+  get '/hometown' do
+    "My hometown is Minneapolis"
+  end
+  
+  get 'favorite-song' do
+    "My favorite song is Hometown Hero"
+  end
   
 end
